bccfe4edc9660faf3940eac79245017c8bd3d773
jan p springer <jsd@igroup.org>
2018-06-14 16:33:03 +0100

    update: gui config

--- init.el
+++ init.el
@@ -27,3 +27,17 @@
     (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
+ '(show-paren-mode t)
+ '(tool-bar-position (quote left)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
