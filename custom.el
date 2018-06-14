c003c81752ac97a1d205dba842fef55e7de6be37
jan p springer <jsd@igroup.org>
2018-06-14 19:20:48 +0100

    added

---
+++ custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(package-selected-packages (quote (magit auctex org)))
+ '(reftex-bibpath-environment-variables
+   (quote
+    ("BIBINPUTS" "TEXBIB" "/home/jsd/Documents/LaTeX/bibliography//")))
+ '(safe-local-variable-values
+   (quote
+    ((TeX-master . \.\./main_lecture)
+     (TeX-master . \.\./\.\./main_lecture))))
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-position (quote left)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ jsd.el
@@ -0,0 +1,7 @@
+;; frame setup
+(setq initial-frame-alist '( (right . 0) (top . 0) (height . 64) (width . 107) ))
+(setq default-frame-alist '( (right . 0) (top . 0) (height . 64) (width . 107) ))
+
+;; default packages to load
+(starter-kit-load "latex")
+
